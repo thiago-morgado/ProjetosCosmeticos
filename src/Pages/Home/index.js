830a0de1dd90c2e5b62945aa257b27821a3b2237
Juliano Nascimento <julianonascimento@MacBook-Pro-de-Juliano.local>
2020-09-13 16:32:48 -0300

    Finalizando Commit

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import {Button} from 'react-native';
-import {Container, Texto, Corpo, Topo, Div} from './styles';
 
-function HomeScreen({navigation}) {
+import {Container, Header, Texto, Slide, Image} from './styles';
+
+export default function index() {
   return (
     <Container>
-      <Corpo>
-        <Topo>
-          <Div>
-            <Texto>Loja</Texto>
-          </Div>
-
-          <Button
-            title="Fazer login"
-            onPress={() => navigation.navigate('login')}
-          />
-        </Topo>
-      </Corpo>
+      <Header>
+        <Texto>Tela Produtos</Texto>
+      </Header>
+      <Slide>
+        <Image source={require('../../assests/images/imagens-01.png')} />
+      </Slide>
     </Container>
   );
 }
-
-export default HomeScreen;
--- src/Pages/Home/styles.js
+++ src/Pages/Home/styles.js
@@ -1,21 +1,29 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
-  background: #fefe;
+export const Container = styled.SafeAreaView`
   flex: 1;
+  background: #fefe;
+`;
+export const Header = styled.View`
+  background: #fff;
+  height: 50px;
+  justify-content: center;
+  align-items: center;
 `;
 export const Texto = styled.Text`
   font-size: 24px;
-  font-weight: bold;
 `;
-export const Corpo = styled.SafeAreaView``;
-
-export const Topo = styled.View`
-  background: #fff;
-  height: 40px;
-  margin-top: -10px;
-`;
-export const Div = styled.View`
+export const Slide = styled.View`
   justify-content: center;
   align-items: center;
+  background: #fff;
+  border-radius: 10px;
+  width: 70%;
+  margin-top: 30px;
+`;
+export const Image = styled.Image`
+  align-self: center;
+  background: transparent;
+  width: 200px;
+  height: 150px;
 `;
